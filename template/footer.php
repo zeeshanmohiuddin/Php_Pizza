ea3a720fda93477b4bd7b8d6ec9ca83c15bd6a1d
zeeshanmohiuddin <40073848+zeeshanmohiuddin@users.noreply.github.com>
2020-11-10 23:26:47 +0500

    Add files via upload

---
+++ template/footer.php
@@ -0,0 +1,10 @@
+
+<footer class="section"> 
+    <div class="center grey-text">
+        copyright 2020 
+    </div>
+</footer>
+
+
+</body>
+
---
+++ template/header.php
@@ -0,0 +1,48 @@
+<head>
+    <title>  Sample Project </title> 
+     <!-- Compiled and minified CSS -->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+     <style class="text/css">
+        .brand{
+            background: #cbb09c !important;
+        }
+
+        .brand-text{
+            color: #cbb09c !important;
+        }
+
+        form{
+            max-width: 460px;
+            margin: 20px auto;
+            padding: 20px;
+        }
+
+        .pizza{
+            width: 100px;
+            margin: 40px auto -30px;
+            display: block;
+            position: relative;
+            top: -30pxl
+        }
+
+      </style>
+</head>
+
+
+<body class="grey lighten-4">
+    <nav class="white depth-0">
+        <div class="container">
+            <a href="index.php" class="brand-logo brand-text"> Pizza Shop </a>
+                <ul id="nav-mobile" class="right hide-on-small-and-down">
+                    <li>
+                        <a href="add.php" class="btn brand z-depth-0"> Add a pizza </a>
+                    </li>
+
+                </ul> 
+        </div>
+
+
+    </nav>
+
+
